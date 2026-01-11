5d456b0a223ed20adc1be065c65349eb2eae9cdd
Dmitriy Belskiy <dm.a.belsky@gmail.com>
2026-01-12 01:27:58 +0700

    Fix Android Kotlin/Java target mismatch

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
   alias(libs.plugins.android.application)
+  alias(libs.plugins.kotlin.android)
   alias(libs.plugins.kotlin.compose)
 }
 
@@ -24,8 +25,8 @@ android {
     }
   }
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
   }
   buildFeatures {
     compose = true
