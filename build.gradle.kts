c797b58782edcdaa7e54dbbc53b041f5ff8c2a68
Dmitriy Belskiy <dm.a.belsky@gmail.com>
2026-01-13 15:26:52 +0700

    Add detekt for KMP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
   alias(libs.plugins.android.application) apply false
@@ -7,4 +10,31 @@ plugins {
   alias(libs.plugins.kotlin.serialization) apply false
   alias(libs.plugins.kotlin.compose) apply false
   alias(libs.plugins.compose.multiplatform) apply false
+  alias(libs.plugins.detekt) apply false
+}
+
+subprojects {
+  plugins.withId("org.jetbrains.kotlin.multiplatform") {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+  }
+  plugins.withId("org.jetbrains.kotlin.android") {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+  }
+
+  plugins.withId("io.gitlab.arturbosch.detekt") {
+    extensions.configure<DetektExtension> {
+      buildUponDefaultConfig = true
+      config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+    }
+    tasks.withType<Detekt>().configureEach {
+      setSource(files(
+        "src/commonMain/kotlin",
+        "src/androidMain/kotlin",
+        "src/desktopMain/kotlin",
+        "src/iosMain/kotlin",
+        "src/wasmJsMain/kotlin",
+        "src/main/kotlin"
+      ))
+    }
+  }
 }
